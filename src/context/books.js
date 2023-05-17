452ec3c9b5530a2d41e0792c89a9890f52b5be97
anhndv <dvanh4@gmail.com>
2023-05-17 10:11:35 +0700

    Update books.js

--- src/context/books.js
+++ src/context/books.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useCallback, useState } from "react";
 import axios from "axios";
 
 const BooksContext = createContext();
@@ -6,10 +6,10 @@ const BooksContext = createContext();
 function Provider({ children }) {
   const [books, setBooks] = useState([]);
 
-  const fetchBooks = async () => {
+  const fetchBooks = useCallback(async () => {
     const response = await axios.get("http://localhost:3002/books");
     setBooks(response.data);
-  };
+  }, []);
 
   const editBookById = async (id, newTitle) => {
     const response = await axios.put(`http://localhost:3002/books/${id}`, {
